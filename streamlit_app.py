245e99ce37452f047ecd9920a8ce1a31072ca0c9
Lucas Colas <lucas.colas13@gmail.com>
2025-07-23 16:42:28 +0200

    Fix error to copy the data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 # Load the fine-tuned model and tokenizer
 model_path = "imdb-distilbert-finetuned"
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # Create a sentiment analysis pipeline
-sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+sentiment_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer, device=0 if torch.cuda.is_available() else -1)
 
 # Streamlit app
 st.title("IMDB Movie Review Sentiment Analysis")
